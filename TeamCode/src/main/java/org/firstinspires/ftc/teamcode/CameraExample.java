4acb096d9843d3dde6d8765c0b48e5f262c242fd
vvila <104012246+dalesnim@users.noreply.github.com>
2026-01-04 22:28:04 +0500

    message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraExample.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+
+@Autonomous
+public class CameraExample extends OpMode {
+    AprilTagCamera cam = new AprilTagCamera();
+
+    @Override
+    public void init() {
+        cam.init(hardwareMap, telemetry);
+
+    }
+
+    @Override
+    public void loop() {
+
+
+        cam.update();
+        AprilTagDetection id20 = cam.getTagBySpecifiedId(20);
+        telemetry.addData("id20 string", id20.toString());
+
+
+    }
+}
